c67a831e7c78d4e64006e6365a0762621a5fc745
komali2 <rogersjcaleb@gmail.com>
2016-10-19 15:39:14 -0700

    images are showing now

--- client/index.js
+++ client/index.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
     $('#getFiles').click(function(){
         $.ajax('server/php').then(function(res){
-            console.log("got response", res);
+            JSON.parse(res)['files'].forEach(function(el, i){
+                appendToSlider(createImageDiv(el.url));
+            });
+            $(".regular").slick({
+                dots: true,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 5,
+                slidesToScroll: 5,
+                responsive: [
+                    {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                    },
+                    {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                    },
+                    {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                    }
+                ]
+            });
         });
     });
 });
@@ -10,7 +44,7 @@ var createImageDiv = function(url){
     var $newElement = $(document.createElement('div'));
     var $newImage = $(document.createElement('img'));
 
-    $newImage.attr("src",url);
+    $newImage.attr("src", url);
 
     $newElement.append($newImage);
     return $newElement;
@@ -18,5 +52,5 @@ var createImageDiv = function(url){
 };
 
 var appendToSlider = function(element){
-    $('regular slider').append(element);
+    $('#slider-area').append(element);
 };
