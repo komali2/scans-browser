e747f961668d4b725980e05bee63ca4452fa330e
komali2 <rogersjcaleb@gmail.com>
2016-10-19 15:24:30 -0700

    wip: appending new pics to slider

--- client/index.js
+++ client/index.js
@@ -5,3 +5,18 @@ $(document).ready(function(){
         });
     });
 });
+
+var createImageDiv = function(url){
+    var $newElement = $(document.createElement('div'));
+    var $newImage = $(document.createElement('img'));
+
+    $newImage.attr("src",url);
+
+    $newElement.append($newImage);
+    return $newElement;
+
+};
+
+var appendToSlider = function(element){
+    $('regular slider').append(element);
+};
